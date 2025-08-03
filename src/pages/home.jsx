8970b52048affd25c4e6d8dccf4d505446193abb
JonJenson <jonjenson2004@gmail.com>
2025-08-03 18:55:34 +0530

    About-weavers corection

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,6 @@ import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import LoginSection from '../components/LoginSection';
 import InfoSection from '../components/InfoSection';
-import WeaversSection from '../components/WeaversSection';
 import Footer from '../components/Footer';
 
 function toggleLogin(){
@@ -16,9 +15,7 @@ const HomePage = () => {
     <>
       <Navbar toggleLogin={toggleLogin} />
       <Hero />
-      
       <InfoSection />
-      <WeaversSection />
       <Footer />
     </>
   )
