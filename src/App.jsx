337fa9144bd93f84d12b8be7d9b67c06dc29fe42
JosseyMarin <josseymarinm.cse2023@citchennai.net>
2025-08-03 18:05:06 +0530

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './pages/home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginSection from './components/LoginSection';
+import WeaverDetails from './pages/weavers_details';
 
 function App() {
   const [showLogin, setShowLogin] = useState(false);
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/supplier-login" element={<LoginSection toggleLogin={toggleLogin} />} />
           <Route path="/weaver-login" element={<weaver_login />} />
+          <Route path="/weaver-details" element={<weavers_details/>}/>
         </Routes>
       </Router>
     </div>
